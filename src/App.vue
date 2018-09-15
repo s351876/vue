cfb73e084eddbada748efcb87cc9c357ba9cc8c0
Dariusz Wasiulewski <d.wasiulewski@safistudio.pl>
2018-09-15 13:19:42 +0200

    [#2.1]commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <my-form />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyForm from './components/MyForm.vue'
+
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MyForm
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1200px;
+  margin: 90px auto;
 }
 </style>
---
+++ src/components/MyForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <b-container>
+      <form @submit.prevent="onSubmit">
+        <h2>Products list</h2>
+        <ul>
+          <li v-for="(product, index) in products" :key="product.id">
+            {{ product.name }} <b-button variant="danger" @click="removeProduct(index)">remove product</b-button>
+          </li>
+        </ul>
+        <p v-if="!products.length">No products!</p>
+        <b-input name="name" v-validate="'required|min:3'" @keyup.enter="onSubmit()" type="text" v-model="name" />
+        <div v-show="errors.has('name')">
+          {{ errors.first('name') }}
+        </div>
+        <b-button variant="success" @click="onSubmit()">Add product</b-button>
+      </form>
+
+    </b-container>
+  </div>
+</template>
+
+<script>
+    import uuid from 'uuid/v4';
+
+    export default {
+      name: 'MyForm',
+      data(){
+        return {
+            name: '',
+            products: [{
+                id: 0,
+                name: 'Coś'
+            }, {
+                id: 1,
+                name: 'Coś2'
+            }]
+        }
+      },
+      methods: {
+          addProduct(){
+              this.products.push(
+                  {
+                      id: uuid,
+                      name: this.name
+                  }
+              )
+              this.name = '';
+              this.$forceUpdate();
+          },
+          removeProduct(index){
+              this.products.splice(index, 1);
+              this.$forceUpdate();
+          },
+          onSubmit() {
+              this.$validator.validateAll().then(result => {
+                  if (!result) {
+                      return;
+                  }
+                  this.addProduct();
+              });
+          }
+      }
+  }
+</script>
+
+<style lang="scss" scoped>
+  ul{
+    list-style: none;
+    padding: 0;
+  }
+  li{
+    padding: 15px 0;
+    border-bottom: 1px #eaeaea solid;
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
